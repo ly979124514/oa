2a36e06f3837b63661d19649eb6da32243f34e74
ly979124514 <ly979124514@163.com>
2017-07-20 17:40:56 +0800

    修改时间显示

--- Framework7-Vue-Webpack-Template/src/pages/email/send_content.vue
+++ Framework7-Vue-Webpack-Template/src/pages/email/send_content.vue
@@ -4,12 +4,13 @@
    <f7-list form>
      <f7-list-item>
        <f7-label>收件人</f7-label>
-       <f7-input @click="test" ref="reciever" type="text" v-model="reciever"/>
-       <!--<f7-input open-popup="#popover-list" />-->
+       <f7-input style="text-align: center" @click="test" ref="reciever" type="text" v-model="reciever"/>
+       <!--<f7-input open-popup="#popover-list"/>-->
      </f7-list-item>
      <f7-list-item>
-       <f7-label>时间</f7-label>
-       <f7-input ref="date_input" v-model="date" type="text" disabled="true"/>
+        <f7-label>时间</f7-label>
+        <!--<f7-input value=this.timeInit v-model="date" type="text" />-->
+        <f7-label>{{ this.time }}</f7-label>
      </f7-list-item>
      <f7-list-item>
        <f7-label>标题</f7-label>
@@ -20,14 +21,14 @@
        <f7-input type="textarea" v-model="content"/>
      </f7-list-item>
      <f7-list-item>
-        <f7-button @click="sendMessage">发送</f7-button><f7-button>返回</f7-button><f7-button @click="test">test</f7-button>
+        <f7-button @click="sendMessage">发送</f7-button><f7-button @click="test">test</f7-button>
      </f7-list-item>
    </f7-list>
    <f7-popup id="popover-list">
       <f7-list contacts>
         <f7-list-group v-for="(group,key) in this.contracts">
           <f7-list-item :title="key" group-title></f7-list-item>
-          <f7-list-item checkbox v-for="object in group" :title="object.USER_NAME" @click="getEaddress(object)"></f7-list-item>
+          <f7-list-item v-for="object in group" :title="object.USER_NAME" @click="getEaddress(object)"></f7-list-item>
         </f7-list-group>
       </f7-list>
    </f7-popup>
@@ -39,6 +40,7 @@ export default {
   data () {
     return {
       reci_name : '',
+      time : '',
       myDate : new Date(),
       apiUri:'http://172.31.0.31:8081/cat/sendMail',
       jsonMail:{"emailContent":"","emailReciever":"","emailSender":"ly979124514@163.com","emailDate":"","emailTitle":""},
@@ -63,13 +65,13 @@ export default {
         console.log(this.mydate);
         console.log(this.content);
         this.jsonMail.emailContent=this.content;
-        this.jsonMail.emailReciever=this.reciever;
         this.jsonMail.emailDate=this.get_timp();
         this.jsonMail.emailTitle=this.title;
-        this.jsonMail.emailSender=logUser.mailAddress;
+        this.jsonMail.emailSender=this.logUser.mailAddress;
         this.json2String=JSON.stringify(this.jsonMail);
         console.log('json2String:'+ this.json2String);
         this.$http.post(this.apiUri,this.json2String).then(function (response){
+             console.log("**************----------------*******************");
              if(this.reciever == null)
              { 
                 f7.alert("请填写收件人地址","发送箱");
@@ -109,7 +111,7 @@ export default {
         return timp;
    },
    pageInit: function() {
-        this.$refs.date_input.value=this.get_timp();
+         this.time = this.get_timp();
    }
   }
 }
